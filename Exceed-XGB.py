da88cbdc4b12c669f3302a4a3e70e697c20b5775
LiWanxia25 <2696204074@qq.com>
2025-04-14 14:06:40 +0800

    Update Exceed-XGB.py

--- Exceed-XGB.py
+++ Exceed-XGB.py
@@ -17,10 +17,10 @@ st.title("Sirolimus High Exposure (>15 ng/mL) Predictor")
 # Define feature names
 feature_names = ['Height', 'HDL', 'TC', 'PLT', 'IL10_rs1800896_CT']
 
-Height = st.number_input("Height (cm):", min_value=50, max_value=180, value=120)
+Height = st.number_input("Height (cm):", min_value=0, max_value=180, value=120)
 HDL = st.number_input("HDL (mmol/L):", min_value=0.00, max_value=3.00, value=1.30)
 TC = st.number_input("TC (mmol/L):", min_value=0.00, max_value=10.00, value=4.00)
-PLT = st.number_input("PLT (109/L):", min_value=0, max_value=10, value=250)
+PLT = st.number_input("PLT (109/L):", min_value=0, max_value=400, value=250)
 IL10_rs1800896_CT = st.selectbox("IL-10.rs1800896_CT:", options=[1, 2], format_func=lambda x: 'No' if x == 1 else 'Yes')
 
 
